9042af435cce7222e8b2ba3ac8a1259c44e84c8e
Kuhlekwethu <kuhle0306@gmail.com>
2025-04-15 22:44:20 +0200

    Add project files.

---
+++ KwethuPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KwethuPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Journey()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Academics()
+        {
+            return View();
+        }
+    }
+}
